fc632e9e9d8593313d3d9834a2f8a5250d980b9c
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-11-25 18:19:37 -0800

    Added starting homework description help

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+
   return `# ${data.title}
+
+  ${generateDescription(data.description)}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+   
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
 
 `;
 }
+
+
+function generateDescription ( description ) {
+
+  return `##Description
+
+  ${data.description}`;
+
+}
+
+console.log(generateMarkdown){
+
+  title: "Testing",
+  description: "Testing description"
+
+}
+
 
 module.exports = generateMarkdown;
