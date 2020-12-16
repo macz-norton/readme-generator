2b48ab0ac576adff34515d8f4076277728bc4335
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-16 14:09:35 -0800

    Updated questions object, set up inquirer prompt

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-
+    {
+        name: "title"
+    },
+    {
+        description: "description"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
 
-}
+    inquirer
+    .prompt(questions)
+    .then(response => {
+
+        const content = generateMarkdown(response);
+
+    })
+
+    }
 
 // function call to initialize program
 init();
