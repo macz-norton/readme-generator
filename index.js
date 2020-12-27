44e320e17237c16b0ebf172c9c02be758dc00f97
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2020-12-27 12:52:10 -0800

    Added the initial inquirer messages

--- index.js
+++ index.js
@@ -3,11 +3,37 @@ const inquirer = require("inquirer");
 // array of questions for user
 const questions = [
     {
-        name: "title"
+        message: "What is your GitHub username?"
     },
     {
-        description: "description"
-    }
+        message: "What is your email address?"
+    },
+    {
+        message: "What is your project name?"
+    },
+    {
+        message: "Please write a short description of your project."
+    },
+    {
+        type: "list",
+        message: "What type of license should your project have?"
+        MIT, APACHE 2.0, GPL 3.0, BSD 3, None
+
+    },
+    {
+        message: "What command should be run to install dependencies?"
+        // add default response of npm i
+    },
+    {
+        message: "What command should be run to run tests?"
+        // add default response of npm test
+    },
+    {
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ];
 
 // function to write README file
